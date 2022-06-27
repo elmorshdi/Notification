f2eea54a8fd7c6369e95497a96986ced6fb05cc2
elmorshdi53 <elmorshdi53@std.mans.edu.eg>
2022-06-27 09:46:42 +0200

    chat Notification

--- app/src/main/java/com/example/notification/MainActivity.kt
+++ app/src/main/java/com/example/notification/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.notification
 
 import android.app.Notification
 import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.support.v4.media.session.MediaSessionCompat
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.RemoteInput
 import com.example.notification.MyApp.Companion.CHANNEL_ONE_ID
 import com.example.notification.databinding.ActivityMainBinding
 
@@ -16,13 +20,26 @@ import com.example.notification.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var notificationManager: NotificationManagerCompat
+    private var mediaSession: MediaSessionCompat? = null
+companion object{
+  M
+    fun messagingWithReplyCompanion() {
+        TODO("Not yet implemented")
+    }
 
+    var MESSAGES :ArrayList<Message> = ArrayList()
+
+}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         notificationManager = NotificationManagerCompat.from(this);
+        mediaSession = MediaSessionCompat(this, "tag")
 
+        MESSAGES.add(Message("Good morning!", "Jim"))
+        MESSAGES.add(Message("Hello", null))
+        MESSAGES.add(Message("Hi!", "Jenny"))
         binding.showNotificationButton.setOnClickListener {
             simple()
         }
@@ -32,7 +49,14 @@ class MainActivity : AppCompatActivity() {
         binding.bigImageNotificationButton.setOnClickListener {
             bigImage()
         }
+        binding.messagingWithReply.setOnClickListener {
+
+            messagingWithReply()
+        }
     }
+
+
+
 
     private fun simple() {
         val activityIntent = Intent(this, MainActivity::class.java)
@@ -92,12 +116,13 @@ class MainActivity : AppCompatActivity() {
               .build()
         notificationManager.notify(2, notification)
     }
-
     private fun bigImage(){
         val activityIntent = Intent(this, MainActivity::class.java)
         val contentIntent = PendingIntent.getActivity(
             this,
-            0, activityIntent, 0
+            0, activityIntent,
+            PendingIntent.FLAG_IMMUTABLE  // setting the mutability flag
+
         )
 
         val picture = BitmapFactory.decodeResource(resources, R.drawable.download)
@@ -121,4 +146,59 @@ class MainActivity : AppCompatActivity() {
 
         notificationManager.notify(3, notification)
     }
-}
+
+     fun messagingWithReply() {
+        val activityIntent = Intent(this, MainActivity::class.java)
+        val contentIntent = PendingIntent.getActivity(
+            this,
+            0, activityIntent,
+            PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT  // setting the mutability flag
+        )
+        val remoteInput: RemoteInput = RemoteInput.Builder("key_text_reply")
+            .setLabel("Your answer...")
+            .build()
+        val replyIntent: Intent
+        var replyPendingIntent: PendingIntent? = null
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            replyIntent = Intent(this, NotificationReceiver::class.java)
+            replyPendingIntent = PendingIntent.getBroadcast(
+                this,
+                0, replyIntent,
+                PendingIntent.FLAG_MUTABLE // setting the mutability flag
+            )
+        } else {
+            //start chat activity instead (PendingIntent.getActivity)
+            //cancel notification with notificationManagerCompat.cancel(id)
+        }
+        val replyAction: NotificationCompat.Action = NotificationCompat.Action.Builder(
+            com.example.notification.R.drawable.ic_baseline_notifications,
+            "Reply",
+            replyPendingIntent
+        ).addRemoteInput(remoteInput).build()
+        val messagingStyle = NotificationCompat.MessagingStyle("Me")
+        messagingStyle.conversationTitle = "Group Chat"
+        for (chatMessage in MESSAGES) {
+            val notificationMessage = NotificationCompat.MessagingStyle.Message(
+                chatMessage.text,
+                chatMessage.timestamp,
+                chatMessage.sender
+            )
+            messagingStyle.addMessage(notificationMessage)
+        }
+        val notification: Notification = NotificationCompat.Builder(applicationContext, CHANNEL_ONE_ID)
+            .setSmallIcon(R.drawable.ic_baseline_notifications)
+            .setStyle(messagingStyle)
+            .addAction(replyAction)
+            .setColor(Color.BLUE)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+            .setContentIntent(contentIntent)
+            .setAutoCancel(true)
+            .setOnlyAlertOnce(true)
+            .build()
+        val notificationManager = NotificationManagerCompat.from(applicationContext)
+        notificationManager.notify(4, notification)
+    }
+    }
+
+
--- app/src/main/java/com/example/notification/NotificationReceiver.kt
+++ app/src/main/java/com/example/notification/NotificationReceiver.kt
@@ -3,12 +3,20 @@ package com.example.notification
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.RemoteInput
 
 
 class NotificationReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent) {
         val message = intent.getStringExtra("toastMessage")
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        val remoteInput: Bundle = RemoteInput.getResultsFromIntent(intent)
+
+        val replyText = remoteInput.getCharSequence("key_text_reply")
+        val answer = Message(replyText!!, null)
+        MainActivity.MESSAGES.add(answer)
+        MainActivity.messagingWithReplyCompanion()
     }
 }
